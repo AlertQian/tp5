7f7014e1e97b902002512664ba8d5e3922921409
AlertQian <1679026896@qq.com>
2018-09-10 16:38:49 +0800

    upload header img

--- application/wap/controller/Upload.php
+++ application/wap/controller/Upload.php
@@ -9,18 +9,27 @@ class Upload extends Controller
     	$file = request()->file('file');
 	    // 移动到框架应用根目录/public/uploads/ 目录下
 	    $image = \think\Image::open($file);
-	    $path=ROOT_PATH . 'uploads';
-	    $name='123.jpg';
-	    $ret= $image->thumb(150, 150)->save($path. DS .$name);
-	    return $ret;
+	    $size = $image->size();
+	    
+	    $thumbpath=ROOT_PATH . DS . '/public/uploads/imgthumb/'.date('Ymd');
+	    if(!is_dir($thumbpath)){
+	    	mkdir($thumbpath);
+	    }
+	    $name=time().rand(100, 1000).'.jpg';
+	    $ret=$image->thumb(300, 300)->save($thumbpath. DS .$name);
+	    $imgpath=ROOT_PATH . DS . '/public/uploads/imgupload';
 	    if($file){
-	        $info = $file->validate(['size'=> 376200 ,'ext'=>'jpg,png,gif'])->move(ROOT_PATH . DS . 'uploads');
+	        $info = $file->validate(['size'=>307200,'ext'=>'jpg,png,gif'])->move($imgpath);
 	        if($info){
-	        	
+	        	$savename= $info->getSaveName();
+	        	$url=$imgpath. DS .$savename;
+	        	$response = array(
+				   'url'  => $url 
+				);
+	        	echo json_encode($response);
 	        }else{
 	            // 上传失败获取错误信息
-	            //echo $file->getError();
-	            echo "noo";
+	            echo $file->getError();
 	        }
 	    }else{
 	    	echo ROOT_PATH . DS.'------';
