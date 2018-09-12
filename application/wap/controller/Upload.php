66410f10dc3b30a3ee95e0d5b15a27edf1029680
Your Name <you@example.com>
2018-09-12 16:11:13 +0800

    add cropper

--- application/wap/controller/Upload.php
+++ application/wap/controller/Upload.php
@@ -10,20 +10,20 @@ class Upload extends Controller
 	    // 移动到框架应用根目录/public/uploads/ 目录下
 	    $image = \think\Image::open($file);
 	    $size = $image->size();
-	    
-	    $thumbpath=ROOT_PATH . DS . '/public/uploads/imgthumb/'.date('Ymd');
+	    $thumbpath=ROOT_PATH . DS . 'public'. DS .'uploads'. DS .'imgthumb'. DS .date('Ymd');
 	    if(!is_dir($thumbpath)){
-	    	mkdir($thumbpath);
+	    	mkdir($thumbpath,0777,true);
 	    }
 	    $name=time().rand(100, 1000).'.jpg';
 	    $ret=$image->thumb(300, 300)->save($thumbpath. DS .$name);
-	    $imgpath=ROOT_PATH . DS . '/public/uploads/imgupload';
+	    $imgpath=ROOT_PATH . DS . 'public'. DS .'uploads'. DS .'imgupload';
 	    if($file){
 	        $info = $file->validate(['size'=>307200,'ext'=>'jpg,png,gif'])->move($imgpath);
 	        if($info){
 	        	$savename= $info->getSaveName();
-	        	$url=$imgpath. DS .$savename;
+	        	$url=DS .'uploads'. DS .'imgupload'. DS .$savename;
 	        	$response = array(
+	        	   'code' => 200,
 				   'url'  => $url 
 				);
 	        	echo json_encode($response);
