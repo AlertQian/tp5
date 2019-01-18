c7ac7bafce8d2d2838c37c562a53153df03ab444
Your Name <you@example.com>
2019-01-18 17:15:56 +0800

    erfe

--- runtime/temp/51aafdea33564326b1bf69391ed1777c.php
+++ runtime/temp/51aafdea33564326b1bf69391ed1777c.php
@@ -1,3 +1,4 @@
+<?php if (!defined('THINK_PATH')) exit(); /*a:3:{s:64:"D:\wamp\www\tp5\public/../application/wap\view\user\myhuifu.html";i:1547801552;s:55:"D:\wamp\www\tp5\application\wap\view\public\header.html";i:1545358047;s:55:"D:\wamp\www\tp5\application\wap\view\public\footer.html";i:1545529423;}*/ ?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -30,12 +31,29 @@
 <script src="/fds/js/jquery-1.11.3.min.js"></script>
 <script src="/static/layui/layui.js"></script>
 <div class="layui-main" style="width: 100%;">
-{include file="public/header" /}
+<div class="header">
+	<a href="javascript:history.back(-1)" id="header_back" class="back">返回</a>
+	<div class="search" id="search_ico" onclick="showNewPage('搜索',searchHtml,newPageSearch);" style="display:none;">搜索</div>
+	<?php if(\think\Session::get('nickname') != null): ?>
+	<a href="<?php echo url('user/index'); ?>" class="my ico_ok" id="login_ico">我的</a>
+	<?php else: ?>
+	<a href="<?php echo url('login/index'); ?>" class="my" id="login_ico">我的</a>
+	<?php endif; ?>
+	<div class="type" id="nav_ico">导航</div>
+	<span id="ipageTitle" style=""><?php echo $title; ?></span>
+	<div class="nav_APP" id="nav_APP" style="display: none;">
+		<ul class="clearfix" id="nav_APP_data2">
+		<li><a href="<?php echo url('wap/index/index'); ?>">首页<s class="s" style="background-image:url(/wap/main/images/nav_icon/headIcon.png);"></s></a></li>
+		<li><a href="">活动<s class="s" style="background-image:url(/wap/main/images/nav_icon/201705251752370917548.png);"></s></a></li>
+		<li><a href="<?php echo url('forum/index'); ?>">社区<s class="s" style="background-image:url(/wap/main/images/nav_icon/201705251752556953922.png);"></s></a></li>
+		</ul><span class="arrow-up"></span>
+	</div>
+</div>
 <div class="wrapper">
 	<div class="layui-tab layui-tab-brief" lay-filter="info">
 	  <ul class="layui-tab-title">
-	    <li><a href="{:url('user/managetie')}">我的发帖</a></li>
-	    <li class="layui-this"><a href="{:url('user/comment')}">我的回复</a></li>
+	    <li><a href="<?php echo url('user/managetie'); ?>">我的发帖</a></li>
+	    <li class="layui-this"><a href="<?php echo url('user/comment'); ?>">我的回复</a></li>
 	  </ul>
 	  <div class="layui-tab-content">
 		<button class="del_btns tpts-btn layui-btn layui-btn-danger">清空全部消息</button>
@@ -58,7 +76,21 @@
 	  </div>
 	</div>
 </div>
-{include file="public/footer" /}
+<div class="foot_link" id="foot_link">
+	<ul class="link">
+	<li><a href="<?php echo url('wap/index/index'); ?>">首页</a></li><!--<li><a href="https://www.mh163k.com/request.ashx?action=iswap&iswap=0">电脑版</a></li>--><li><a href="https://www.mh163k.com/help/article_13.html">联系我们</a></li><!--<li><a href="http://app.163k.com/download.aspx?id=12299" data-img="../UploadFile/index/2015/9-7/201509071154513472674.png">客户端</a></li>--><li><a href="https://www.mh163k.com/service/">反馈留言</a></li>
+	</ul>
+	<!--163k地方门户网站系统：<a href="https://www.mh163k.com/">mh163k.com</a>　皖ICP备11020229号-->Copyright @ 2003-2018 mh163k.com
+	<input type="hidden" name="nickname" value="<?php echo \think\Session::get('nickname'); ?>">
+</div>
+<script>
+$(function(){
+	$('#nav_ico').click(function(e){
+		e.preventDefault();
+		$('#nav_APP').fadeToggle('fast');
+	});
+});
+</script>
 </div>
 <script type="text/javascript">
 	layui.use(['element','layer'], function(){
@@ -68,7 +100,7 @@
         $(".comt-del").click(function(){
         	$id=$(this).attr('id');
         	layer.confirm('确定删除?',{icon: 3, title:'提示'},function(index){
-        		$.post("{:url('user/delcomt')}",{id:$id},function(data){
+        		$.post("<?php echo url('user/delcomt'); ?>",{id:$id},function(data){
         			if(data.code ==1){
 		              layer.msg(data.msg,{icon: 1,anim: 6, time: 2000},function(){
 		                location.href=data.url;
